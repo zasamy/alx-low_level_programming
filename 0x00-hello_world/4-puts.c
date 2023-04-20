f1bfbae2c21f0fdc1dbb0b0daaf6232dce00e6e7
zasamy <zainebzahiri@gmail.com>
2023-04-20 15:26:58 -0700

    4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
-*main - entry point
-*retourn: always 0 (Success)
+ * main - Entry point
+ *
+ * Descreption: A C program that print with put function
+ *
+ * Retourn: Always 0 (Success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	retourn (0);
+	retourn(0);
 }
