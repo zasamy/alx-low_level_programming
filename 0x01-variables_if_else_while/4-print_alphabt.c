af9cd41b777ef1865b7486b43385781d25d83fef
zasamy <zainebzahiri@gmail.com>
2023-04-21 08:57:21 -0700

    4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphabet in lowercase except q and e
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+				putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
