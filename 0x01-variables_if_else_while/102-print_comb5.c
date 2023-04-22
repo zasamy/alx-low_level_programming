652c822dbfc9a34d655ee62ce7f0513fc3826f19
zasamy <zainebzahiri@gmail.com>
2023-04-21 18:44:22 -0700

    102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: program that prints all
-   possible different combinations of two digits
+ * possible different combinations of two digits
  *
  * Return: Always 0 (success)
 */
