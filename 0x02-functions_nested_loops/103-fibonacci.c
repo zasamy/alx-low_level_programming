5f9676a3af93bb55eb00bc2b3834a3800960dcd9
zasamy <zainebzahiri@gmail.com>
2023-04-25 13:01:14 -0700

    103-fibonacci.c file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print the sum of even-valued Fibonacci sequence
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	unsigned long i = 0, j = 1, total_ sum;
+	float  sum;
+
+	while (1)
+	{
+		sum = i + j;
+		if (total_sum > 4000000)
+			break;
+		if ((total_sum % 2) == 0)
+			sum += total_sum;
+		i = j;
+		j = total_sum;
+	}
+	printf("%.0f\n", sum);
+
+	return (0);
+}
