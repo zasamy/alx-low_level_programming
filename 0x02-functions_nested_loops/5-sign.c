f871c4436a9c510366f50ce6bc90a1467b8d2057
zasamy <zainebzahiri@gmail.com>
2023-04-25 08:39:06 -0700

    5-sign.c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_signe - Description if input number greater than zero,
+ * iqual or less than zero
+ *
+ *@n: takes integer type input for function
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
+*/
+
+int print_signe(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
+
