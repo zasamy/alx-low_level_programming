cffb7576b3db45e151a0d6831fbcc31c19e229e0
zasamy <zainebzahiri@gmail.com>
2023-04-25 06:35:30 -0700

    1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print alphabet using the _putchar function
+ *
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	putchar('\n');
+}
