08a73833763217b7c161a921756b542c9e3fae5a
zasamy <zainebzahiri@gmail.com>
2023-04-25 10:38:06 -0700

    11-print_to_98.c file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 		for (i = n; i > 98; i--)
 			printf("%d, ", i);
 	else
-		for (i = n; i < 98, i++)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
 	printf("98\n");
 }
