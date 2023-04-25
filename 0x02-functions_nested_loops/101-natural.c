b803ba3a105cf5454c8ed525762e5301cd2846d3
zasamy <zainebzahiri@gmail.com>
2023-04-25 11:20:09 -0700

    101-natural.c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: computes the sum of all the multiples of 3 or 5
+ *bellow 1024
+ *
+ * return: always (0)
+*/
+
+int main(void)
+{
+	int sum, n;
+
+	sum = 0;
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+
+			sum = sum + n;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
