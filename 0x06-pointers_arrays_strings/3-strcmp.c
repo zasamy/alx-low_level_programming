33128ba92ec180f5b9effc981212dee7c73e144f
zasamy <zainebzahiri@gmail.com>
2023-05-04 07:45:04 -0700

    3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 1 if true 0 if fals
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int comp = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			comp = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (comp);
+}
