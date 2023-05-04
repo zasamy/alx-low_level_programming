9490e1b295878f0c734712ffab414ce471843782
zasamy <zainebzahiri@gmail.com>
2023-05-04 11:45:06 -0700

    101-print_number.c file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,16 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num;
+
+	num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = n;
+		num = -n;
 	}
-	if (num / 10 > 0)
+	if (num / 10 != 0)
 	{
 		print_number(num / 10);
 	}
