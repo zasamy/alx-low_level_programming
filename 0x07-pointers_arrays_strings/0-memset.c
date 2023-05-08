b04543e9a8b135bc9e391386395b6d2e61debb9c
zasamy <zainebzahiri@gmail.com>
2023-05-08 08:00:59 -0700

    0-memset.c file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * *_memset - fills memory with a constant byte.
+ *
+ * @s: pointer to the block of memory file
+ * @b: value to be set
+ * @n: the number of bytes to be set to the value
+ * Return: pointer s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i, j;
+
+	for (j = n; j > 0; j--)
+	{
+		for (i = 0; i <= j; i++)
+			s[i] = b;
+	}
+	return (s);
+}
