f69dbcbadf244588373b20ca3ed1deaedf687df1
zasamy <zainebzahiri@gmail.com>
2023-05-12 09:49:19 -0700

    0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: integer
+ * @argv: list
+ *
+ * Return: 0
+ */
+int main(int argc, char const **argv)
+{
+	void(argc);
+	printf("%s\n", argv[0]);
+	return (0);
+}
