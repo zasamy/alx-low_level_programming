cacea0cdcba6c834f41a596470c17b29db85f628
zasamy <zainebzahiri@gmail.com>
2023-05-24 11:45:55 -0700

    1-array_iterator.c file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,9 +13,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *end;
-
-	*end = &array + size - 1;
+	int *end = array + size - 1;
 
 	if (array && size && action)
 		while (array <= end)
