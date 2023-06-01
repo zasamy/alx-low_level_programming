fb882bc8a0ebcfca912b5b8f1c5026d9854d6a07
zasamy <zainebzahiri@gmail.com>
2023-06-01 08:51:37 -0700

    1-list_len.c file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the first node
+ *
+ * Return: size of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int element = 0
+
+		while (h != NULL)
+		{
+			h = h->next;
+			element++;
+		}
+	return (elemetnt);
+}
+
