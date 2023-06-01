8bbc536befef40e61715481e0f78835db093e9ff
zasamy <zainebzahiri@gmail.com>
2023-06-01 10:15:23 -0700

    100-first.c file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((com¡nstructor));
+
+/**
+ * constructor - function with constractor
+ *
+ * Return: void
+ */
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
