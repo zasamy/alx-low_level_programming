9e7884dd46c0b8f3b21248d43a25e0fe21cada29
zasamy <zainebzahiri@gmail.com>
2023-08-22 16:48:34 -0700

    1-dlistint_len.c file

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * dlistint_len - numbers of elements in list
+ * @h: adress of head node
+ * Return: numer of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
